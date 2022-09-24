78e161b1d7c05eb81da15ce8b2184f4d71693e1f
Ess_Weber <aessotchossim@gmail.com>
2022-09-24 17:40:17 +0200

    create org problem

--- api/organisation/create.php
+++ api/organisation/create.php
@@ -2,7 +2,7 @@
   // Headers
   header('Access-Control-Allow-Origin: *');
   header('Content-Type: application/json');
-  header('Access-Control-Allow-Methods: POST');
+  header('Access-Control-Allow-Methods: *');
   header('Access-Control-Allow-Headers: Access-Control-Allow-Origin, Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
   header('Access-Control-Allow-Credentials: true');
 
